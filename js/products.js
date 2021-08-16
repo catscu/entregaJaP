deb14e87384b4191412b0c92f325bbdbd33cd0e1
catscu <88331081+catscu@users.noreply.github.com>
2021-08-16 13:04:42 -0300

    corregí en productos de artículos a artículos vendidos

--- js/products.js
+++ js/products.js
@@ -16,7 +16,7 @@ function showCategoriesList(array){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                        <small class="text-muted">` + category.soldCount + ` artículos vendidos</small>
                     </div>
                     <h3 class="mb-1">`+ category.currency + ' ' + category.cost +`</h3>
                     <div>
