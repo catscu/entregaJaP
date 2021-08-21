bfa71cbea5532f9343db14c5ff0af479d8722cf6
catscu <88331081+catscu@users.noreply.github.com>
2021-08-21 16:31:50 -0300

    agregué la autenticación con Google

--- js/login.js
+++ js/login.js
@@ -1,4 +1,28 @@
 
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    location.href = "index.html";
+    alert("Conectado");
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
 function verificar(){
 
     let dato = document.getElementById('user');
@@ -18,6 +42,7 @@ function verificar(){
 
 function desconectar(){
     localStorage.clear();
+    signOut();
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -26,6 +51,11 @@ function desconectar(){
 document.addEventListener("DOMContentLoaded", function (e) {
     let usuario = JSON.parse(localStorage.getItem('usuario'));
 
-
+    /*
+    if(usuario.estado === 'conectado'){
+        location.href = "index.html";
+    }
+    si logeo, al usar desconectar no me deja entrar de vuelta a login.html igual
+    */
 
 });
